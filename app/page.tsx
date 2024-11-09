d3f978b6b87f7b1959d83f663c247e30d9266645
Thiago Filipe Oliveira <61127389+filipeoliveira93@users.noreply.github.com>
2024-11-09 08:22:30 -0300

    chore: add tailwind prettier plugin

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 const Home = () => {
-  return( <h1>Olá eu sou o projeto</h1>)
+  return( <h1 className="text-3xl font-bold underline text-red-100">Olá eu sou o projeto</h1>)
 }
 
 export default Home
